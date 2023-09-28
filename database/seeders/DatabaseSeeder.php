3926e696617bfb3b041ad23b3121a0754452d735
Reza <alirezaarifi69@gmail.com>
2023-09-28 16:17:40 +0700

    Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\InformationPermit;
 use App\Models\User;
+use App\Models\StatusPermit;
+use App\Models\Permit;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -19,6 +22,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'Super Admin',
             'nip' => '99999999999',
             'password' => Hash::make('password')
+        ]);
+        StatusPermit::insert([
+            ['name' => 'Menunggu Persetujuan'],
+            ['name' => 'Ditolak'],
+            ['name' => 'Diterima'],
+        ]);
+
+        InformationPermit::insert([
+            ['name' => 'Sakit'],
+            ['name' => 'Izin'],
+            ['name' => 'Cuti'],
+            ['name' => 'Meninggal'],
         ]);
     }
 }
